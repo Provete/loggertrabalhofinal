52d00f63602559b950497a0fd740c1a31c76527a
gustavo provete <guprovete@gmail.com>
2024-09-15 16:01:35 -0300

    Adicionado a função de logError

--- src/main/java/org/example/Logger/CSVLogger.java
+++ src/main/java/org/example/Logger/CSVLogger.java
@@ -45,7 +45,8 @@ public class CSVLogger implements Logger
                     logEntry.getNomeUsuario(),
                     logEntry.getData(),
                     logEntry.getHora(),
-                    logEntry.getNomeUsuarioAutenticado()
+                    logEntry.getNomeUsuarioAutenticado(),
+                    "\n"
             );
 
             printer.flush();
@@ -54,5 +55,49 @@ public class CSVLogger implements Logger
             e.printStackTrace();
         }
 
+    }
+
+    @Override
+    public void logErro(String mensagemErro, Operacao operacao, String nomeUsuario, LocalDateTime dataEHora, String nomeUsuarioAutenticado)
+    {
+        LogEntry logEntry = new LogEntry(mensagemErro, operacao, nomeUsuario, dataEHora, nomeUsuarioAutenticado);
+
+        String caminhoAbsolutoArquivo = Paths.get(caminhoArquivo).toAbsolutePath().toString();
+        File arquivo = new File(caminhoAbsolutoArquivo);
+
+        if(!arquivo.exists())
+        {
+            try
+            {
+                arquivo.getParentFile().mkdirs();
+                arquivo.createNewFile();
+            } catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+
+        try(BufferedWriter writer = Files.newBufferedWriter(
+                arquivo.toPath(),
+                StandardOpenOption.APPEND,
+                StandardOpenOption.CREATE))
+        {
+            CSVPrinter printer = new CSVPrinter(writer, CSVFormat.newFormat(';'));
+
+            printer.printRecord(
+                    logEntry.getMensagemErro(),
+                    logEntry.getOperacao(),
+                    logEntry.getNomeUsuario(),
+                    logEntry.getData(),
+                    logEntry.getHora(),
+                    logEntry.getNomeUsuarioAutenticado(),
+                    "\n"
+            );
+
+            printer.flush();
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/org/example/Logger/JSONLogger.java
+++ src/main/java/org/example/Logger/JSONLogger.java
@@ -53,5 +53,42 @@ public class JSONLogger implements Logger
         } catch(Exception e) { e.printStackTrace(); }
     }
 
+    @Override
+    public void logErro(String mensagemErro, Operacao operacao, String nomeUsuario, LocalDateTime dataEHora, String nomeUsuarioAutenticado)
+    {
+        LogEntry logEntry = new LogEntry(mensagemErro, operacao, nomeUsuario, dataEHora, nomeUsuarioAutenticado);
+
+        String caminhoAbsolutoArquivo = Paths.get(caminhoArquivo).toAbsolutePath().toString();
+        File arquivo = new File(caminhoAbsolutoArquivo);
+
+        if(!arquivo.exists())
+        {
+            try
+            {
+                arquivo.getParentFile().mkdirs();
+                arquivo.createNewFile();
+            } catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+
+        List<LogEntry> logEntries;
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectWriter writer = mapper.writer(new DefaultPrettyPrinter());
+
+        try
+        {
+            logEntries = mapper.readValue(arquivo, new TypeReference<List<LogEntry>>() {});
+        } catch (IOException e) { logEntries = new ArrayList<LogEntry>(); e.printStackTrace(); }
+
+        logEntries.add(logEntry);
+
+        try
+        {
+            writer.writeValue(arquivo, logEntries);
+        } catch(Exception e) { e.printStackTrace(); }
+    }
+
 
 }
